bb942651f219b96f732486c070b93df67c2c66e2
Dónal Murray <donal.murray@cern.ch>
2017-02-16 12:14:11 +0000

    Tidy up code

--- modelsim/active_control/data_processor/data_processor_units/flagger.vhdl
+++ modelsim/active_control/data_processor/data_processor_units/flagger.vhdl
@@ -1,72 +1,53 @@
--- Isolation tagger
+-- flagger.vhdl
 -- Author Ben Jeffrey, Nicholas Mead
 -- Date Created 09/02/2016
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
-USE work.Isolation_Flagging_Package.all;
-USE work.Detector_Constant_Declaration.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.isolation_flagging_package.all;
+use work.detector_constant_declaration.all;
 
-ENTITY Flagger IS
-	
-  PORT(
+entity flagger is
+port(
+   	rst 			: IN 	std_logic;	
+   	clk			: IN 	std_logic;
+   	data_in			: IN 	datatrain;
+   	data_out		: OUT 	datatrain
+);
 
-   	rst 			: in 	std_logic;	
-   	clk			: in 	std_logic;
-   	data_in			: in 	datatrain;
-   	data_out		: out 	datatrain
-  );
+end entity;
 
- END ENTITY;
-
-ARCHITECTURE a OF Flagger IS
-	
-	SHARED VARIABLE inter_reg : datatrain;
-
-BEGIN
-	
-	PROCESS(clk, rst)
-	BEGIN
-
-		IF rst = '1' THEN
-
+architecture a of flagger is
+	shared variable inter_reg : datatrain;
+begin
+	process(clk, rst)
+	begin
+		if rst = '1' then
 			data_out <= reset_pattern_train;
+		elsif rising_edge(clk) then
+			-- propagate first and last SPP - these are always edge cases, so not flagged
+			inter_reg(0) 			:= data_in(0);
+			inter_reg(MAX_FLAG_SIZE-1)	:= data_in(MAX_FLAG_SIZE-1);
 
-		ELSIF rising_edge(clk) THEN
-
-			-- propogate first and last SPP - these are always edge cases, so not flagged
-			inter_reg(0) := data_in(0);
-			inter_reg(MAX_FLAG_SIZE-1) := data_in(MAX_FLAG_SIZE-1);
-
-			FOR i IN 1 to (MAX_FLAG_SIZE - 2) LOOP
-			
-				-- if next SPP is all zeros, must be edge case, so don't flag
-				IF (data_in(i+1) = x"00_00_00_00") THEN
-
+			for i in 1 to (MAX_FLAG_SIZE - 2) loop
+				-- if next SPP is all zeroes, must be edge case, so don't flag
+				if (data_in(i+1) = x"00_00_00_00") then
 					inter_reg(i) := data_in(i);
-
-				ELSE 
-
+				else
 					-- check if isolated by seeing if neighbouring BCID signals are present 
-					IF (to_integer(unsigned(data_in(i)(13 downto 8))) - to_integer(unsigned(data_in(i-1)(13 downto 8))) > 1) AND 
-						(to_integer(unsigned(data_in(i+1)(13 downto 8))) - to_integer(unsigned(data_in(i)(13 downto 8))) > 1) THEN
-
+					if (to_integer(unsigned(data_in(i)(13 downto 8))) - to_integer(unsigned(data_in(i-1)(13 downto 8))) > 1) AND 
+					(to_integer(unsigned(data_in(i+1)(13 downto 8))) - to_integer(unsigned(data_in(i)(13 downto 8))) > 1) then
 						inter_reg(i) := data_in(i) OR x"80_00_00_00";
-
-					ELSE
-
+					else
 						inter_reg(i) := data_in(i);
-
-					END IF;
-				END IF;
-			END LOOP;
-
-		END IF;
+					end if;
+				end if;
+			end loop;
+		end if;
 
 		-- pass internal register to the output
 		data_out <= inter_reg;
-
-	END PROCESS;
-END a;
+	end process;
+end a;
 
