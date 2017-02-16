e10d874a4bca2435e22f5063f901aeac613f7e14
Dónal Murray <donal.murray@cern.ch>
2017-02-16 16:22:06 +0000

    Tidy up code

--- modelsim/isolation_flagging_package.vhd
+++ modelsim/isolation_flagging_package.vhd
@@ -2,25 +2,22 @@
 -- Author Nicholas Mead
 -- Date Created 19/11/2015
 
--- IEEE VHDL standard library:
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
-USE work.Detector_Constant_Declaration.all; 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.detector_constant_declaration.all; 
 
 -- Define Package
-PACKAGE Isolation_Flagging_Package IS 
-
+package isolation_flagging_package is 
 	-- type def for array of std logic vectors
-	TYPE dataTrain 		IS ARRAY(MAX_FLAG_SIZE - 1 downto 0) OF std_logic_vector(31 downto 0);
-	TYPE dataTrain_rd 	IS ARRAY(7 downto 0) OF std_logic_vector(RD_WORD_SIZE - 1 downto 0);
-	TYPE dataTrain_wr 	IS ARRAY(7 downto 0) OF std_logic_vector(WR_WORD_SIZE - 1  downto 0);
+	type datatrain 			is array(MAX_FLAG_SIZE - 1 downto 0)	of std_logic_vector(31 downto 0);
+	type datatrain_rd 		is array(7 downto 0) 			of std_logic_vector(RD_WORD_SIZE - 1 downto 0);
+	type datatrain_wr 		is array(7 downto 0) 			of std_logic_vector(WR_WORD_SIZE - 1 downto 0);
 	
 
-	CONSTANT reset_pattern_spp    : std_logic_vector(31 downto 0) := (others => '0');
-	CONSTANT reset_pattern_train  : dataTrain := (others => reset_pattern_spp);
+	constant reset_pattern_spp    	: std_logic_vector(31 downto 0) 		:= (others => '0');
+	constant reset_pattern_train  	: datatrain 					:= (others => reset_pattern_spp);
 	
-	CONSTANT reset_pattern_wr	  : std_logic_vector(WR_WORD_SIZE - 1 downto 0) := (others => '0');
-	CONSTANT reset_pattern_wrtrain: dataTrain_wr := (others => reset_pattern_wr);
-
-END Isolation_Flagging_Package;
+	constant reset_pattern_wr	: std_logic_vector(WR_WORD_SIZE - 1 downto 0) 	:= (others => '0');
+	constant reset_pattern_wrtrain	: datatrain_wr 					:= (others => reset_pattern_wr);
+end isolation_flagging_package;
