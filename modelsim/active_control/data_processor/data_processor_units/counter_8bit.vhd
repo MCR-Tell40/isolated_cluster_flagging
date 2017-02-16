124a8a8b6cf223136990f8f6b1e16e4c90ca8164
Dónal Murray <donal.murray@cern.ch>
2017-02-16 16:20:49 +0000

    Tidy up code

--- modelsim/active_control/data_processor/data_processor_units/counter_8bit.vhd
+++ modelsim/active_control/data_processor/data_processor_units/counter_8bit.vhd
@@ -2,53 +2,35 @@
 -- Author Nicholas Mead
 -- Date Created 19/11/2015
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
-USE IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-USE work.detector_constant_declaration.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use work.detector_constant_declaration.all;
 
-ENTITY counter_8bit IS
-	PORT(
-		clk 	: IN std_logic;
-		rst 	: IN std_logic;
-		en  	: IN std_logic;
-		count 	: OUT std_logic_vector(DATA_SIZE_MAX_BIT - 1 downto 0)
-		);
-END ENTITY;
+entity counter_8bit is
+port(
+	clk, rst, en	: IN 	std_logic;
+	count 		: OUT	std_logic_vector (DATA_SIZE_MAX_BIT - 1 downto 0)
+);
+end entity;
 
-
-ARCHITECTURE a OF counter_8bit IS
-
-		SIGNAL inter_reg : std_logic_vector(DATA_SIZE_MAX_BIT - 1 downto 0);
+architecture a of counter_8bit is
+	signal inter_reg : std_logic_vector (DATA_SIZE_MAX_BIT - 1 downto 0);
 	
-BEGIN
-
-	PROCESS (clk,rst,en)
-	
-	BEGIN
-
-		IF (rst = '1') THEN
-
-			inter_reg <= x"00";
-
-		ELSIF (rising_edge(clk) AND en = '1') THEN
-
-			IF (inter_reg = x"FF") THEN
-
-				inter_reg <= x"00";
-
-			ELSE
-
-				inter_reg <= inter_reg + 1;
-
-			END IF;
-
-		END IF;
-
-	END PROCESS;
-
-	count <= inter_reg;
-
-END a; 
+begin
+	process(clk, rst, en)
+	begin
+		if (rst = '1') then
+			inter_reg 	<= x"00";
+		elsif (rising_edge(clk) AND en = '1') then
+			if (inter_reg = x"FF") then
+				inter_reg 	<= x"00";
+			else
+				inter_reg 	<= inter_reg + 1;
+			end if;
+		end if;
+	end process;
+	count 	<= inter_reg;
+end a;
