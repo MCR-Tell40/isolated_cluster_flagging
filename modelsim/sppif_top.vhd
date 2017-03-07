754415b45eebf79ceb4b5565b91ab0469e38c4bf
Dónal Murray <donal.murray@cern.ch>
2017-03-07 16:55:11 +0000

    Change bcid port types of active controller. Still need to define a new pipe for the current bcid

--- modelsim/sppif_top.vhd
+++ modelsim/sppif_top.vhd
@@ -15,42 +15,42 @@ entity sppif_top is
 	port(	clk, rst	: IN	std_logic;
 
 		-- train size ram interface
-		ct_addr		: OUT	std_logic_vector(8 downto 0);				-- number of addresses
-		ct_data		: IN	std_logic_vector(COUNT_RAM_WORD_SIZE - 1 downto 0);	-- number of 
+		ct_addr		: OUT	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);		-- current bcid
+		ct_data		: IN	std_logic_vector(COUNT_RAM_WORD_SIZE - 1 downto 0);	-- number of spps in this bcid's ram
 
 		-- from ram
 		rd_en		: OUT	std_logic;						-- read enable
-		rd_addr		: OUT	std_logic_vector(RD_RAM_ADDR_SIZE - 1 downto 0);	-- addresses of SPPs to be input
-		rd_data		: INOUT	std_logic_vector(RD_WORD_SIZE - 1 downto 0);		-- SPP data input
+		rd_addr		: OUT	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);		-- bcid of SPPs to be input
+		rd_data		: IN	std_logic_vector(RD_WORD_SIZE - 1 downto 0);		-- SPP data input
 
 		-- to output
 		wr_en		: OUT	std_logic;						-- write enable
-		wr_addr		: OUT	std_logic_vector(WR_RAM_ADDR_SIZE - 1 downto 0);	-- addresses of SPPs to be output
-		wr_data		: INOUT	std_logic_vector(WR_WORD_SIZE - 1 downto 0));		-- SPP data output
+		wr_addr		: OUT	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);		-- addresses of SPPs to be output
+		wr_data		: OUT	std_logic_vector(WR_WORD_SIZE - 1 downto 0));		-- SPP data output
 end sppif_top;
 
 architecture a of sppif_top is
 -- internal signal pipes
 	-- active controller pipes
 	signal ac_en_pipe		: std_logic;
-	signal ct_addr_pipe		: std_logic_vector(8 downto 0);
+	signal ct_addr_pipe		: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	signal ct_data_pipe		: std_logic_vector(COUNT_RAM_WORD_SIZE - 1 downto 0);
 	-- from router
 	signal ac_rd_en_pipe		: std_logic;
-	signal ac_rd_addr_pipe		: std_logic_vector(RD_RAM_ADDR_SIZE - 1 downto 0);
+	signal ac_rd_addr_pipe		: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	signal ac_rd_data_pipe		: std_logic_vector(RD_WORD_SIZE - 1 downto 0);
 	-- to mep
 	signal ac_wr_en_pipe 		: std_logic;
-	signal ac_wr_addr_pipe		: std_logic_vector(WR_RAM_ADDR_SIZE - 1 downto 0);
+	signal ac_wr_addr_pipe		: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	signal ac_wr_data_pipe		: std_logic_vector(WR_WORD_SIZE - 1 downto 0);
 
 	-- fifo pipes
 	-- from active controller
 	signal fifo_wr_en_pipe		: std_logic;
-	signal fifo_wr_data_pipe	: std_logic_vector(6 downto 0);
+	signal fifo_wr_data_pipe	: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	-- to bypass controller
 	signal fifo_rd_en_pipe		: std_logic;
-	signal fifo_rd_data_pipe 	: std_logic_vector(6 downto 0);
+	signal fifo_rd_data_pipe 	: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	signal fifo_empty_pipe 		: std_logic;
 
 	-- bypass controller pipes
@@ -58,11 +58,11 @@ architecture a of sppif_top is
 	signal bypass_en_pipe 		: std_logic;
 	-- from router
 	signal by_rd_en_pipe		: std_logic; 
-	signal by_rd_addr_pipe		: std_logic_vector(RD_RAM_ADDR_SIZE - 1 downto 0);
+	signal by_rd_addr_pipe		: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	signal by_rd_data_pipe 		: std_logic_vector(RD_WORD_SIZE - 1 downto 0);
 	-- to mep
 	signal by_wr_en_pipe 		: std_logic;
-	signal by_wr_addr_pipe 		: std_logic_vector(WR_RAM_ADDR_SIZE - 1 downto 0);
+	signal by_wr_addr_pipe 		: std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 	signal by_wr_data_pipe 		: std_logic_vector(WR_WORD_SIZE - 1 downto 0);
 
 
@@ -76,58 +76,51 @@ architecture a of sppif_top is
 
 			-- from router
 			rd_en			: OUT	std_logic;
-			rd_addr			: OUT	std_logic_vector(RD_RAM_ADDR_SIZE - 1 downto 0);
+			rd_addr			: OUT	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 			rd_data			: IN	std_logic_vector(RD_WORD_SIZE - 1 downto 0);
 
 			-- to mep
 			wr_en			: OUT 	std_logic;
-			wr_addr 		: OUT 	std_logic_vector(WR_RAM_ADDR_SIZE - 1 downto 0);
+			wr_addr 		: OUT 	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 			wr_data 		: OUT	std_logic_vector(WR_WORD_SIZE - 1 downto 0);
 
 			-- to fifo
 			fifo_en 		: OUT 	std_logic;
-			fifo_data		: OUT 	std_logic_vector(6 downto 0);
+			fifo_data		: OUT 	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 
 			-- to bypass controller
 			bypass_en 		: OUT 	std_logic);
 	end component;
 
 	component interface_fifo is
-		generic( constant DATA_WIDTH	: 	positive := 7;
-			constant FIFO_DEPTH	: 	positive := 32);
-
 		port(	clk, rst		: IN	std_logic;
 			empty, full		: OUT 	std_logic;
 
 			-- from active controller
 			wr_en			: IN  	std_logic;
-			wr_data			: IN  	std_logic_vector(DATA_WIDTH - 1 downto 0);
+			wr_data			: IN  	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 
 			-- to bypass controller
 			rd_en			: IN  	std_logic;
-			rd_data			: OUT 	std_logic_vector(DATA_WIDTH - 1 downto 0));
+			rd_data			: OUT 	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0));
 	end component;
 
 	component bypass_controller is
-		generic( ADDR_PER_RAM 		: 	integer := 32;
-			MAX_RAM_ADDR_STORE 	: 	integer := 512;
-			SPP_PER_ADDR 		: 	integer := 16);
-		
 		port(	clk, rst, en 		: IN 	std_logic;
 	
 			-- from router
 			rd_en			: OUT 	std_logic;
-			rd_addr 		: OUT 	std_logic_vector(RD_RAM_ADDR_SIZE - 1 downto 0);
+			rd_addr 		: OUT 	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 			rd_data 		: IN 	std_logic_vector(RD_WORD_SIZE - 1 downto 0);
 				
 			-- to mep
 			wr_en			: OUT 	std_logic;
-			wr_addr 		: OUT 	std_logic_vector(WR_RAM_ADDR_SIZE - 1 downto 0);
+			wr_addr 		: OUT 	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 			wr_data 		: OUT	std_logic_vector(WR_WORD_SIZE - 1 downto 0);
 	
 			-- from fifo
 			fifo_rd_en 		: OUT 	std_logic;
-			fifo_data		: IN  	std_logic_vector(6 downto 0);
+			fifo_data		: IN  	std_logic_vector(SPP_BCID_WIDTH - 1 downto 0);
 			fifo_empty  		: IN 	std_logic);
 	end component;
 
@@ -201,18 +194,18 @@ architecture a of sppif_top is
 	begin
 		if bypass_en_pipe = '1' then
 			-- bypass this block
-			rd_addr <= by_rd_addr_pipe;
-    			rd_data <= by_rd_data_pipe;
-    			rd_en 	<= by_rd_en_pipe;
+			rd_addr 	<= by_rd_addr_pipe;
+    			by_rd_data_pipe <= rd_data;
+    			rd_en 		<= by_rd_en_pipe;
 	
-    			wr_addr <= by_wr_addr_pipe;
-    			wr_data <= by_wr_data_pipe;
-    			wr_en 	<= by_wr_en_pipe;
+    			wr_addr 	<= by_wr_addr_pipe;
+    			wr_data 	<= by_wr_data_pipe;
+    			wr_en 		<= by_wr_en_pipe;
     		else 
 			-- pass to active control
-    			rd_addr <= ac_rd_addr_pipe;
-    			rd_data <= ac_rd_data_pipe;
-    			rd_en 	<= ac_rd_en_pipe;
+    			rd_addr 	<= ac_rd_addr_pipe;
+    			ac_rd_data_pipe <= rd_data;
+    			rd_en 		<= ac_rd_en_pipe;
 
     			wr_addr <= ac_wr_addr_pipe;
     			wr_data <= ac_wr_data_pipe;
