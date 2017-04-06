17a064178e3361f7d7b8c014d5f1fa64749113b6
Dónal Murray <donal.murray@cern.ch>
2017-04-06 10:53:31 +0100

    Fix length mismatches and use constant declaration files and packages

--- modelsim/isolated_cluster_flagging_top/icf_processor/flagger.vhd
+++ modelsim/isolated_cluster_flagging_top/icf_processor/flagger.vhd
@@ -1,13 +1,19 @@
 library ieee;
-use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
-use work.detector_constant_declaration.all;	-- constants file
-use work.sppif_package.all;			-- custom type definitions
+use ieee.numeric_std.all;
+
+library work;
+use work.AMC40_pack.all;
+use work.Constant_Declaration.all;
+use work.detector_constant_declaration.all;
+
+library work;
+use work.GDP_pack.all;
 
 entity flagger is
 	port(
 		rst 			: in	std_logic;
-   		clk				: in 	std_logic;
+   		clk			: in 	std_logic;
    		i_data			: in 	spp_array;
    		o_data			: out 	spp_array
 	);
@@ -21,22 +27,22 @@ begin
 	process(clk, rst)
 	begin
 		if rst = '1' then
-			o_data <= (others => '0');
+			o_data <= (others => x"00000000");
 		elsif rising_edge(clk) then
 			-- propagate first and last SPP - these are always edge cases, so not flagged
 			s_data(0) 				<= i_data(0);
-			s_data(MAX_ADDR - 1)	<= i_data(MAX_ADDR - 1);
+			s_data(15)				<= i_data(15);
 
-			for i in 1 to (MAX_ADDR - 2) loop
+			for i in 1 to (14) loop
 				-- if next spp is all zeroes, must be edge case, so don't flag
-				if (i_data(i+1) = x"00_00_00_00") then
+				if (i_data(i+1) = x"00000000") then
 					s_data(i) <= i_data(i);
 				else
 					-- check if isolated by seeing if the columns to either side are empty
 					if (to_integer(unsigned(i_data(i)(13 downto 8))) - to_integer(unsigned(i_data(i-1)(13 downto 8))) > 1) AND
 					(to_integer(unsigned(i_data(i+1)(13 downto 8))) - to_integer(unsigned(i_data(i)(13 downto 8))) > 1) then
 						-- cluster is isolated, flag by setting the MSB to 1
-						s_data(i) <= i_data(i) OR x"80_00_00_00";
+						s_data(i) <= i_data(i) OR x"80000000";
 					else
 						-- cluster is not isolated
 						s_data(i) <= i_data(i);
